698748d222049d17acf0f6d65d4007e951597e9a
Karyna Aliushkevich <89156053+KarynaAliushkevich14@users.noreply.github.com>
2022-12-21 17:10:41 +0100

    2 product, css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { IProduct } from './models/product';
+import { products as data } from './data/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-app';
+  title = 'App components';
+
+  /** Если прописываем тут products, то можем использовать их в app.component.html */
+  products: IProduct[] = data
+
 }
